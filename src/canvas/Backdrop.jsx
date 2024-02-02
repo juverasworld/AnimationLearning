4d460607e56cfa096283e2781b66ac59e0ed0f77
Vee <ifebuchejulia@gmail.com>
2024-02-02 10:42:01 +0100

    content added

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -3,9 +3,21 @@
  import { useRef } from "react"
  import { AccumulativeShadows, RandomizedLight } from "@react-three/drei"
  export default function Backdrop(){
+    const shadows = useRef()
     return(
-        <div className="">
-            Backdrop
-        </div>
+      <AccumulativeShadows
+      ref={shadows}
+      temporal
+      frames={60}
+      alphaTest={0.85}
+      scale={10}
+      rotation={[Math.PI / 2, 0, 0]}
+      position={[0, 0, -0.14]}
+      >
+        <RandomizedLight
+        // amount={4}
+
+        />
+      </AccumulativeShadows>
     )
  }
