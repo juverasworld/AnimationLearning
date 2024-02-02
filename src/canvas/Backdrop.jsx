86558778f89b336bd55c40aa375162ce7f7a0da3
Vee <ifebuchejulia@gmail.com>
2024-02-02 09:48:02 +0100

    Added some animation content

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -1,3 +1,7 @@
+ import { easing } from "maath"
+ import { useFrame } from "@react-three/fiber"
+ import { useRef } from "react"
+ import { AccumulativeShadows, RandomizedLight } from "@react-three/drei"
  export default function Backdrop(){
     return(
         <div className="">
--- src/canvas/CameraRig.jsx
+++ src/canvas/CameraRig.jsx
@@ -1,31 +1,37 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
-import {easing} from "maath";
+import { easing } from "maath";
 import { useSnapshot } from "valtio";
 
 import { Decal, useGLTF, useTexture } from "@react-three/drei";
 import state from "../store";
 
-export default function CameraRig({children}){
-    const group = useRef( ); 
-const snap = useSnapshot(state)
-useFrame((state, delta)=>{
+export default function CameraRig({ children }) {
+  const group = useRef();
+  const snap = useSnapshot(state);
+  useFrame((state, delta) => {
+    const isBreakPoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+    //set the inintial postion of the model
+    let targetPosition =[-0.4, 0, 2];
+    if(snap.intro){
+        if(isBreakPoint) targetPosition =[0, 0,2];
+        if(isMobile) targetPosition =[0, 0.2,2.5];
+    }else{
+        if(isMobile) targetPosition =[0, 0.2,2.5];
+        else targetPosition =[0, 0,2];
+    }
 
-    const isBreakPoint = window.innerWidth <=1260
-    const isMobile = window.innerWidth <=600;
-    // 
+    // set model camera position
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta)
     // set the model smoothly
     easing.dampE(
-        group.current.rotation,
-        [state.pointer.y/10, -state.pointer.x/5, 0],
-        0.25,
-        delta
-    ) 
-})
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    );
+  });
 
-    return(
-     <group ref={group}>
-        {children}
-     </group>
-    )
- }
+  return <group ref={group}>{children}</group>;
+}
